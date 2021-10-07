f45f9a261fcdb3af2b10754ce85d2eb1d4b1a95b
Jostein Tysse <josteiht@stud.ntnu.no>
2021-10-07 16:39:02 +0200

    Add basic advert model

--- backend/apps/adverts/models.py
+++ backend/apps/adverts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class AdvertType(models.TextChoices):
+    IS_SITTER = 'IS_SITTER'
+    NEED_SITTER = 'ND_SITTER'
+
+
+class Advert(models.Model):
+
+    advertType = models.CharField(max_length=9,
+                                  choices=AdvertType.choices, default=AdvertType.NEED_SITTER, blank=False)
+
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, blank=False)
+
+    content = models.TextField()
+
+    date = models.DateTimeField(blank=False)
--- backend/apps/adverts/views.py
+++ backend/apps/adverts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Advert, AdvertType
+from .serializers import AdvertSerializer
 
-# Create your views here.
+
+class NeedSitterAdvertView(generics.ListCreateAPIView):
+    queryset = Advert.objects.filter(advertType=AdvertType.NEED_SITTER)
+
+    serializer_class = AdvertSerializer
+
+    permission_classes = []
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class IsSitterAdvertView(generics.ListCreateAPIView):
+    queryset = Advert.objects.filter(advertType=AdvertType.IS_SITTER)
+
+    serializer_class = AdvertSerializer
+
+    permission_classes = []
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
