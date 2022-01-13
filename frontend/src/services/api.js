92ccb0fb8f961e59716bc56f9d6e9888b2d30c1c
Jakob Notland <jakob.notland@ntnu.no>
2022-01-13 15:59:14 +0100

    Bugfix, prevent constant attempts to refresh authentication tokens

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -28,7 +28,7 @@ instance.interceptors.response.use(
   async (err) => {
     const originalConfig = err.config;
 
-    if (originalConfig.url !== "/login/" && err.response) {
+    if (originalConfig.url !== "/login/" && err.response && originalConfig.url !== "/refresh/") {
       // Access Token was expired
       if (err.response.status === 401 && !originalConfig._retry) {
         originalConfig._retry = true;
